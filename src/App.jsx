a94bce37696954196583d6413dedd6f21bd85623
Murat Çolak <mo939614@gmail.com>
2025-03-16 23:04:36 +0300

    düzenleme için silindi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import RegistirationPage from './components/RegistrationForm/RegistrationForm';
 import LoginPage from './components/LoginForm/LoginForm';
-import RightSideBar from "./components/RightSideBar/RightSideBar.jsx";
 
 const App = () => {
   return (
     <div>
       <RegistirationPage />
       <LoginPage />
-      <RightSideBar />
     </div>
   );
 };
