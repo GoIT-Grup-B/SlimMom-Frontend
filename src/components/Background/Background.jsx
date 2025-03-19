fb429250cbb9055b9080b50dfd0190a8b950a596
gizemcoban <gizem.coban.r@gmail.com>
2025-03-19 10:39:21 +0100

    svg fix

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,6 +1,6 @@
 const Background = ({children}) => {
   return (
-    <div className="md:bg-[url('src/assets/svg/banana.svg'),url('src/assets/svg/strawberry.svg'),url('src/assets/svg/green.svg')] md:bg-[65%,35%,90%]  bg-no-repeat  md:bg-[position:right_bottom,right_center,right_top]  bg-contain min-h-screen xl:bg-[url('src/assets/svg/fruitss.svg')] bg-no-repeat bg-right bg-contain min-h-screen" >
+    <div className="md:bg-[url('src/assets/svg/banana.svg'),url('src/assets/svg/strawberry.svg'),url('src/assets/svg/green.svg')] md:bg-[65%,35%,80%]  bg-no-repeat  md:bg-[position:right_bottom,right_center,right_top] bg-contain min-h-screen xl:bg-[url('src/assets/svg/fruitss.svg')] bg-no-repeat bg-right bg-contain min-h-screen" >
         {children}
     </div>
   )
