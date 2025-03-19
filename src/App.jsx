6d0ebc5856cf2312ace2aa054b8c147fe25ec435
gizemcoban <gizem.coban.r@gmail.com>
2025-03-19 10:25:28 +0100

    background component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import RegistirationPage from './components/RegistrationForm/RegistrationForm';
 import LoginPage from './components/LoginForm/LoginForm';
 import RightSideBar from './components/RightSideBar/RightSideBar';
 import Background from './components/Background/Background';
+//import AppRoutes from './routers/AppRoutes';
 
 const App = () => {
   return (
@@ -11,8 +12,12 @@ const App = () => {
       <LoginPage />
       <RightSideBar />
       </Background>
+
       </div>
   );
 };
 
 export default App;
+//      <main>
+// <AppRoutes/>
+// </main>
