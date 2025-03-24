3ffb5bccac25ee9e55f270433d20d41c68972543
Murat Çolak <mo939614@gmail.com>
2025-03-24 18:10:48 +0300

    mainden çekildi backgraund.jsx

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,12 +1,7 @@
 const Background = ({ children }) => {
   return (
-<<<<<<< HEAD
     <div className="md:bg-[url('/svg/banana.svg'),url('/svg/strawberry.svg'),url('/svg/green.svg')] md:bg-[65%,35%,80%]  md:bg-[position:right_bottom,right_center,right_top] xl:bg-[url('/svg/fruitss.svg')] bg-no-repeat bg-right bg-contain min-h-screen">
       {children}
-=======
-    <div className="md:bg-[url('/svg/banana.svg'),url('/svg/strawberry.svg'),url('/svg/green.svg')] md:bg-[65%,35%,80%]  bg-no-repeat  md:bg-[position:right_bottom,right_center,right_top] bg-contain min-h-screen xl:bg-[url('/svg/fruitss.svg')] bg-no-repeat bg-right bg-contain min-h-screen" >
-        {children}
->>>>>>> development
     </div>
   );
 };
